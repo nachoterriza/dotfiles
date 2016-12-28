85dbc690ce60202d20b2058229b31ba7b4641024
Nacho <iterriza@ucm.es>
2016-12-28 16:41:33 +0100

    bm

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,6 +5,8 @@ externo=HDMI1
 
 if xrandr | grep "$externo disconnected"; then
 	xrandr --output "$externo" --off --output "$interno" --auto
+	feh --bg-scale /home/nacho/Imágenes/ocean.jpg
 else
 	xrandr --output "$interno" --off --output "$extern" --auto
+	feh --bg-scale /home/nacho/Imágenes/Legend/'Blu-ray 3.jpg'
 fi
