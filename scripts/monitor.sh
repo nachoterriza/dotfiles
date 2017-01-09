5650ebdbd55df9e099eb2098bd62055a1ef594df
Nacho <iterriza@ucm.es>
2017-01-10 00:27:30 +0100

    new nvidia checker

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,10 +5,10 @@ externo=HDMI1
 
 if xrandr | grep "$externo disconnected"; then
 	xrandr --output "$externo" --off --output "$interno" --auto
-	feh --bg-scale /home/nacho/Imágenes/ocean.jpg
+	firejail feh --bg-scale /home/nacho/Imágenes/ocean.jpg
 	/home/nacho/scripts/checkBatt.sh
 else
 	xrandr --output "$interno" --off --output "$extern" --auto
-	feh --bg-scale /home/nacho/Imágenes/MAC/sierra.jpg
+	firejail feh --bg-scale /home/nacho/Imágenes/MAC/sierra.jpg
 	pactl load-module module-bluez5-discover
 fi
