3c39a7ba9095980e96737d06a0779daa5c130370
Nacho <iterriza@ucm.es>
2017-01-11 01:05:43 +0100

    yes

--- scripts/checkBatt.sh
+++ scripts/checkBatt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BATTINFO=`acpi -b`
 while true; do
-	if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]];
-	then DISPLAY=:0.0 /usr/bin/notify-send -u critical " Low battery!!" "$BATTINFO"
+	if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 90:15:00 ]];
+	then DISPLAY=:0.0 rofi -e "Bateria baja!! `acpi -b`"
 	fi
 	sleep 420
 done
