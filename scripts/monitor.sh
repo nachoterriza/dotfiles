7fa847c1bbee17ec753608571c68c3c2a161b0ab
Nacho <iterriza@ucm.es>
2017-02-19 16:30:52 +0100

    new conky bar

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,8 +5,10 @@ externo=HDMI1
 
 if xrandr | grep "$externo disconnected"; then
 	xrandr --output "$externo" --off --output "$interno" --auto
+    cp /home/nacho/.config/i3/opts/edp1 /home/nacho/.config/i3/conky
 	/home/nacho/scripts/checkBatt.sh
 else
 	xrandr --output "$interno" --off --output "$extern" --auto
+    cp /home/nacho/.config/i3/opts/hdmi /home/nacho/.config/i3/conky
 	pactl load-module module-bluez5-discover
 fi
