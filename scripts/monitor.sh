afa7129b460526dc748db992cff0857f133ce302
Nacho <iterriza@ucm.es>
2017-02-19 12:27:20 +0100

    new wallpper

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -5,10 +5,8 @@ externo=HDMI1
 
 if xrandr | grep "$externo disconnected"; then
 	xrandr --output "$externo" --off --output "$interno" --auto
-	firejail feh --bg-scale /home/nacho/Imágenes/ocean.jpg
 	/home/nacho/scripts/checkBatt.sh
 else
 	xrandr --output "$interno" --off --output "$extern" --auto
-	firejail feh --bg-scale /home/nacho/Imágenes/MAC/sierra.jpg
 	pactl load-module module-bluez5-discover
 fi
