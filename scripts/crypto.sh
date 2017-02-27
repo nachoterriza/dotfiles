92192974527f76e4823f309a709e45545f8f5028
Nacho <iterriza@ucm.es>
2017-02-27 22:54:10 +0100

    crypto

--- scripts/crypto.sh
+++ scripts/crypto.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-SUDO_PASSWD="$(zenity --password --title=Sudo)\n" 2> /dev/null
-CR_PASSWD="$(zenity --password --title=UnlockCrypto)\n" 2> /dev/null
-echo -e $SUDO_PASSWD | echo -e $CR_PASSWD | sudo -S cryptsetup --type luks open /dev/sda3 crypto
-mkdir -p /tmp/crypto
-echo -e $SUDO_PASSWD | sudo -S mount -t ext4 /dev/mapper/crypto /tmp/crypto
-echo -e $SUDO_PASSWD | sudo -S chown -R nacho:users /tmp/crypto
-if [ -e /tmp/crypto/README.md ]; then
-	zenity --info --title="Correcty Unlocked" --text="Sistema descifrado. Montado en /tmp/crypto" 2> /dev/null
+if [ "$(id -ur)" != "0" ]; then
+    echo -e "Se necesitan permisos de administrador"
+    exit 1
 else
-	zenity --error --title="Uncorrecty Unlocked" --text="Ha habido un error" 2> /dev/null
+    cryptsetup --type luks open /dev/sda3 crypto
+    mkdir -p /tmp/crypto
+    mount -t ext4 /dev/mapper/crypto /tmp/crypto
+    chown -R nacho:users /tmp/crypto
+    if [ -e /tmp/crypto/README.md ]; then
+    	echo -e "Sistema descifrado. Montado en /tmp/crypto"
+    else
+    	echo -e "Ha habido un error"
+    fi
 fi
