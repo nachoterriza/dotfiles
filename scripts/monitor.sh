730af9ca4ab8bcefa953cc6bd8019f2d11553e54
Nacho <iterriza@ucm.es>
2017-03-01 23:25:27 +0100

    rofi

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -6,9 +6,11 @@ externo=HDMI1
 if xrandr | grep "$externo disconnected"; then
 	xrandr --output "$externo" --off --output "$interno" --auto
     cp /home/nacho/.config/i3/opts/edp1 /home/nacho/.config/i3/conky
+    cp /home/nacho/.config/rofi/opts/edp1 /home/nacho/.config/rofi/config
 	/home/nacho/scripts/checkBatt.sh
 else
 	xrandr --output "$interno" --off --output "$extern" --auto
     cp /home/nacho/.config/i3/opts/hdmi /home/nacho/.config/i3/conky
+    cp /home/nacho/.config/rofi/opts/hdmi /home/nacho/.config/rofi/config
 	pactl load-module module-bluez5-discover
 fi
