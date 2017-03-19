d59bbeb4498eeedf8fdb5d62f97578e1816441f5
Nacho <iterriza@ucm.es>
2017-03-19 15:29:43 +0100

    foo

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -12,6 +12,6 @@ else
 	xrandr --output "$interno" --off --output "$extern" --auto
     cp /home/nacho/.config/i3/opts/hdmi /home/nacho/.config/i3/conky
     cp /home/nacho/.config/rofi/opts/hdmi /home/nacho/.config/rofi/config
-    python2 /usr/lib/python2.7/site-packages/searx/webapp.py &
+#    python2 /usr/lib/python2.7/site-packages/searx/webapp.py &
 	pactl load-module module-bluez5-discover
 fi
