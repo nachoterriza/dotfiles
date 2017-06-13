f928ba305af76fbf6378ba3302f19bbcbf332f35
Nacho <iterriza@ucm.es>
2017-06-13 18:33:31 +0200

    deleted scripts

--- scripts/checkBatt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BATTINFO=`acpi -b`
-while true; do
-	if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]];
-	then DISPLAY=:0.0 rofi -e "Bateria baja!! `acpi -b`"
-	fi
-	sleep 420
-done
